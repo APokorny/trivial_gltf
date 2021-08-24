6004ebb7fa359dc0750d63963033c70d629d9450
Andreas Pokorny <andreas.pokorny@siemens.com>
2021-08-24 11:50:31 +0200

    Adding preparating code

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# trivial_gltf
+# trivial\_gltf
+
 Trivial GLTF loading code
+
+Neither much to see here no clue how this will turn out.
+
+## License
+
+See license file - this code is provided through the boost software license.
+
+## Misty goal of this library 
+
+I try to make that easy integrateable and flexible to use, so that the calling code is in control of when streams are consumed and how they are processed - avoiding allocation or storing too much state.
---
+++ include/trivial_gltf/gltf_parse.h
@@ -0,0 +1,12 @@
+/* ==========================================================================
+ Copyright (c) 2021 Andreas Pokorny
+ Distributed under the Boost Software License, Version 1.0. (See accompanying
+ file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+========================================================================== */
+
+#ifndef TRIVIAL_GLTF_GLTF_PARSE_H_INCLUDED
+#define TRIVIAL_GLTF_GLTF_PARSE_H_INCLUDED
+namespace trivial_gltf
+{
+}
+#endif
